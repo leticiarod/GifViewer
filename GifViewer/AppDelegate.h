590e3b94c1a23a8cff9718803ac3f0dc53fb899f
leticiarod <leticiamrgs@gmail.com>
2017-10-16 09:58:26 -0300

    Initial Commit

---
+++ GifViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GifViewer
+//
+//  Created by Leticia Rodriguez on 10/16/17.
+//  Copyright © 2017 Leticia Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GifViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GifViewer
+//
+//  Created by Leticia Rodriguez on 10/16/17.
+//  Copyright © 2017 Leticia Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
